6730a69f17250eef4d344d615aceeddbb63d62ee
EJSohn <5on.3un.joo@gmail.com>
2016-09-19 00:51:24 +0900

    find meterial error

--- project/app-rank-dashboard/app/app/app.component.ts
+++ project/app-rank-dashboard/app/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'main',
-  templateUrl: 'app/app/app.component.html'
+  templateUrl: 'app/app/app.component.html',
+  styleUrls: ['app/app/app.component.css']
 })
 
 export class AppComponent { }
