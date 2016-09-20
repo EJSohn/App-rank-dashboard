d304ef05cf2da30de8157225d6d1e1b74171d767
EJSohn <5on.3un.joo@gmail.com>
2016-09-20 18:33:23 +0900

    edit mr-flog component

---
+++ project/app-rank-dashboard/app/mrFlog/mr-flog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'dashboard',
+    templateUrl: 'app/mrFlog/mr-flog.component.html',
+    styleUrls: ['app/mrFlog/mr-flog.component.css']
+})
+
+export class MrFlogComponent {
+    constructor() { }
+}
