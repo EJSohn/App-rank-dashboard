1a304ede6e82b57203897442004ba1867c1e3c08
EJSohn <5on.3un.joo@gmail.com>
2016-09-20 16:59:59 +0900

    add sample pie graph

--- project/app-rank-dashboard/app/pieGraph/piegraph.component.ts
+++ project/app-rank-dashboard/app/pieGraph/piegraph.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 
+// import test static data
+import { DATA } from '../static/piegraph-data';
+
 @Component({
     selector: 'pie-graph',
     templateUrl: 'app/pieGraph/piegraph.component.html',
     styleUrls: ['app/pieGraph/piegraph.component.css']
 })
 
-export class PieGraphComponent { }
+export class PieGraphComponent { 
+    constructor() { }
+
+    ngOnInit(): void {
+        var ctx = document.getElementById("PieGraph").getContext("2d");
+
+        var PieGraph = new Chart(ctx, {
+            data: DATA,
+            type: 'polarArea',
+        });
+        
+    }
+}
---
+++ project/app-rank-dashboard/app/static/piegraph-data.ts
@@ -0,0 +1,26 @@
+export const DATA: any= {
+    datasets: [{
+        data: [
+            11,
+            16,
+            7,
+            3,
+            14
+        ],
+        backgroundColor: [
+            "#FF6384",
+            "#4BC0C0",
+            "#FFCE56",
+            "#E7E9ED",
+            "#36A2EB"
+        ],
+        label: 'My dataset' // for legend
+    }],
+    labels: [
+        "LifeStyles",
+        "Games",
+        "Business",
+        "Book",
+        "Education"
+    ]
+};
