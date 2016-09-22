a00ad845dc8472d7135ec01ede096759b86da7fe
EJSohn <5on.3un.joo@gmail.com>
2016-09-22 15:22:35 +0900

    make mr-flog component's template

--- project/app-rank-dashboard/app/app/app.component.ts
+++ project/app-rank-dashboard/app/app/app.component.ts
@@ -6,11 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['app/app/app.component.css']
 })
 
-
+// main component of app rank dashboard.
 export class AppComponent {
 
   constructor() { }
 
+
+  // makes mr.flog jump twice. 
   jumpFlog(): void {
     for(var i=0; i<2; i++){
       $(".mascot-frog").effect('bounce',{times:2}, 500);
