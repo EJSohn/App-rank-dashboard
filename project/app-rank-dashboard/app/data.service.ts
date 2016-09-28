adb68ddd990011954e4a6fbe9e834416974a22e7
EJSohn <5on.3un.joo@gmail.com>
2016-09-28 22:30:58 +0900

    register pie graph service

--- project/app-rank-dashboard/app/data.service.ts
+++ project/app-rank-dashboard/app/data.service.ts
@@ -8,8 +8,19 @@ export class DataService {
 
     // header for http request. 
     private headers = new Headers({'Content-Type': 'application/json'});
+    private apiUrl = '';
 
     constructor(private http: Http) { }
 
+    getPieGraph(): Promise<any> {
+        return this.http.get(this.apiUrl)
+                .toPromise()
+                .then(response => response.json().data)
+                .catch(this.handleError);
+    }
 
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
 }
