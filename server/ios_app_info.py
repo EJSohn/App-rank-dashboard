0ad7a915a10fabfa6e19cae7cbd91049dc3c6225
EJSohn <5on.3un.joo@gmail.com>
2016-09-29 20:56:01 +0900

    make whole_category function

--- server/ios_app_info.py
+++ server/ios_app_info.py
@@ -10,3 +10,17 @@ url = 'https://itunes.apple.com/kr/rss/toppaidapplications/limit=100/xml'
 soup = BeautifulSoup(urllib2.urlopen(url), "xml")
 
 entries = soup.find_all('entry')
+
+
+def whole_category(language='label'):
+    # return category names in top rank 100.
+
+    """
+    Args: 
+        language: can be 'label'(=korean category name) 
+        or 'term'(english category name). default is label.
+    """
+    category_list = [ entry.category[language] for entry in entries ]
+    category_kind  = list(set(category_list))
+
+    return category_kind
