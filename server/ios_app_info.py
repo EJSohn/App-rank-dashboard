ad896505b3dfe6109fe397229759f51c556e89d5
EJSohn <5on.3un.joo@gmail.com>
2016-09-29 21:12:11 +0900

    make top_ten_category function

--- server/ios_app_info.py
+++ server/ios_app_info.py
@@ -13,14 +13,37 @@ entries = soup.find_all('entry')
 
 
 def whole_category(language='label'):
-    # return category names in top rank 100.
-
     """
     Args: 
         language: can be 'label'(=korean category name) 
         or 'term'(english category name). default is label.
+    Returns:
+        a list consist of category names in top rank 100.
+        example:
+            ['category_name',
+             'category_name2',
+             ...]
     """
     category_list = [ entry.category[language] for entry in entries ]
-    category_kind  = list(set(category_list))
 
-    return category_kind
+    return category_list
+
+
+def top_ten_category():
+    """
+    Returns:
+        a list consist of category name and count in top category rank 10. 
+        example:
+            [{'name':category_name, 'counts':number},
+             {'name':category_name2, 'counts':number2},
+             ...]
+    """
+
+    category_list = whole_category()
+    counts = Counter(category_list)
+
+    top_ten_tuple = counts.most_common(10)
+    top_ten_list = [ { 'name': category[0], 'counts': category[1] } \
+                      for category in top_ten_tuple  ]
+
+    return top_ten_list
