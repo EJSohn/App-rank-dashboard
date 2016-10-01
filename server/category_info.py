bc983a3fdc81e2036e97e72671ff326c2bd12118
EJSohn <5on.3un.joo@gmail.com>
2016-10-01 22:32:37 +0900

    create category handler class and populate functions

--- server/category_info.py
+++ server/category_info.py
@@ -2,48 +2,56 @@ from bs4 import BeautifulSoup
 from collections import Counter
 import lxml, urllib2
 
-# declare common data
 
-# url that retrieving rss feed for ios paid app in top rank 100
-url = 'https://itunes.apple.com/kr/rss/toppaidapplications/limit=100/xml'
-# crate soup object for xml parsing
-soup = BeautifulSoup(urllib2.urlopen(url), "xml")
-
-entries = soup.find_all('entry')
+class NationTypeError(Exception):
+    self.msg = 'nation has to be kr or us'
+    def __str__(self):
+        return self.msg
 
 
-def whole_category(language='label'):
-    """
-    Args: 
-        language: can be 'label'(=korean category name) 
-        or 'term'(english category name). default is label.
-    Returns:
-        a list consist of category names in top rank 100.
-        example:
-            ['category_name',
-             'category_name2',
-             ...]
-    """
-    category_list = [ entry.category[language] for entry in entries ]
+class CategoryHandler:
+    def __init__(self, nation):
+        if (nation!='kr' or nation!='us'):
+            raise NationTypeError
+        else:
+            # url that retrieving rss feed for ios paid app in top rank 100
+            url = 'https://itunes.apple.com/'+nation+'/rss/toppaidapplications/limit=100/xml'
+            # crate soup object for xml parsing
+            self.soup = BeautifulSoup(urllib2.urlopen(url), "xml")
+            self.entries = soup.find_all('entry')
 
-    return category_list
+    def whole_category(language='label'):
+        """
+        Args: 
+            language: can be 'label'(=korean category name) 
+            or 'term'(english category name). default is label.
+        Returns:
+            a list consist of category names in top rank 100.
+            example:
+                ['category_name',
+                'category_name2',
+                ...]
+        """
+        category_list = [ entry.category[language] for entry in self.entries ]
+
+        return category_list
 
 
-def top_ten_category():
-    """
-    Returns:
-        a list consist of category name and count in top category rank 10. 
-        example:
-            [{'name':category_name, 'counts':number},
-             {'name':category_name2, 'counts':number2},
-             ...]
-    """
+    def top_ten_category():
+        """
+        Returns:
+            a list consist of category name and count in top category rank 10. 
+            example:
+                [{'name':category_name, 'counts':number},
+                {'name':category_name2, 'counts':number2},
+                ...]
+        """
 
-    category_list = whole_category()
-    counts = Counter(category_list)
+        category_list = self.whole_category()
+        counts = Counter(category_list)
 
-    top_ten_tuple = counts.most_common(10)
-    top_ten_list = [ { 'name': category[0], 'counts': category[1] } \
-                      for category in top_ten_tuple  ]
+        top_ten_tuple = counts.most_common(10)
+        top_ten_list = [ { 'name': category[0], 'counts': category[1] } \
+                        for category in top_ten_tuple  ]
 
-    return top_ten_list
+        return top_ten_list
